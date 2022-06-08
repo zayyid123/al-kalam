04bdb90eabf7f28c1e4de82c3ef93654797b60e3
zayyid123 <mochamadzayyid@gmail.com>
2022-06-08 23:49:16 +0700

    fiks home

--- src/pages/home/mobile/index.js
+++ src/pages/home/mobile/index.js
@@ -6,6 +6,8 @@ import lastRead from '../../../assets/img/last-read.png'
 import quran from '../../../assets/img/quran.png'
 import quran2 from '../../../assets/img/quran2.png'
 import quran3 from '../../../assets/img/quran3.png'
+import bookmarkQuran from '../../../assets/img/bookmark-quran.png'
+import bulan from '../../../assets/img/bulan.png'
 
 const Mobile = () => {
     return (
@@ -20,34 +22,59 @@ const Mobile = () => {
             {/* last read */}
             <div className='flex justify-between items-center drop-shadow-2xl bg-gradient-to-r from-[#2AB2AF] to-[#7DC694] mx-4 p-3 rounded-3xl'>
                 <div>
-                    <p className='font-thin text-xs text-gray-300 mb-3'>Last Read</p>
+                    <p className='font-thin text-[0.5rem] text-white mb-3'>Last Read</p>
                     <h1 className='font-semibold text-white text-sm'>Ar-Rahman</h1>
-                    <p className='font-thin text-xs text-gray-300 '>Verse No.1</p>
+                    <p className='font-thin text-[0.5rem] text-white '>Verse No.1</p>
                     <p className='font-normal text-xs text-white '>Go to {'>'}</p>
                 </div>
                 <div>
-                    <img className='w-[70px] h-[70px]' src={lastRead} alt='icon last read' />
+                    <img className='w-[90px] h-[90px]' src={lastRead} alt='icon last read' />
                 </div>
             </div>
 
             {/* all */}
             <div className='grid grid-cols-2 gap-4 m-4'>
                 {/* quran */}
-                <div className='bg-gradient-to-b from-[#3295ac] to-[#4fc8e4] rounded-3xl drop-shadow-2xl p-3 h-[140px] text-white row-span-2'>
+                <div className='bg-gradient-to-b from-[#3295ac] to-[#4fc8e4] rounded-3xl drop-shadow-2xl p-3 h-[200px] text-white row-span-2'>
                     <div>
-                        <img className='w-[50px] h-[50px]' src={quran2} alt='icon quran 2' />
+                        <img className='w-[75px] h-[75px]' src={quran2} alt='icon quran 2' />
                     </div>
+                    <br />
                     <br />
                     <div>
                         <h1 className='font-semibold text-white text-sm'>Quran</h1>
                         <p className='font-normal text-xs text-white '>Go to {'>'}</p>
                     </div>
                 </div>
-                <div className='bg-gradient-to-b from-[#c55fa8] to-[#CC80B7] rounded-3xl drop-shadow-2xl p-3 h-[120px] text-white'>2</div>
-                <div className='bg-gradient-to-b from-[#5274c9] to-[#8AA5E9] rounded-3xl drop-shadow-2xl p-3 h-[140px] text-white row-span-2'>3</div>
-                <div className='bg-gradient-to-b from-[#7446ac] to-[#A781D7] rounded-3xl drop-shadow-2xl p-3 h-[120px] text-white'>
+
+                {/* reminder */}
+                <div className='bg-gradient-to-b from-[#c55fa8] to-[#CC80B7] rounded-3xl drop-shadow-2xl p-3 h-[150px] text-white'>
                     <div>
-                        <img className='w-[50px] h-[50px] mt-[-10px] mb-3' src={quran3} alt='icon quran 3' />
+                        <img className='w-[58px] h-[58px] mt-1 mb-6' src={bulan} alt='icon bulan' />
+                    </div>
+                    <div>
+                        <h1 className='font-semibold text-white text-sm'>Memorize</h1>
+                        <p className='font-normal text-xs text-white '>Go to {'>'}</p>
+                    </div>
+                </div>
+
+                {/* bookmarks */}
+                <div className='bg-gradient-to-b from-[#5274c9] to-[#8AA5E9] rounded-3xl drop-shadow-2xl p-3 h-[200px] text-white row-span-2'>
+                    <div>
+                        <img className='w-[70px] h-[70px] mb-3' src={bookmarkQuran} alt='bookmark quran' />
+                    </div>
+                    <br />
+                    <br />
+                    <div>
+                        <h1 className='font-semibold text-white text-sm'>Bookmarks</h1>
+                        <p className='font-normal text-xs text-white '>Go to {'>'}</p>
+                    </div>
+                </div>
+
+                {/* tajwid */}
+                <div className='bg-gradient-to-b from-[#7446ac] to-[#A781D7] rounded-3xl drop-shadow-2xl p-3 h-[150px] text-white'>
+                    <div>
+                        <img className='w-[80px] h-[80px] mt-[-10px] mb-3' src={quran3} alt='icon quran 3' />
                     </div>
                     <div>
                         <h1 className='font-semibold text-white text-sm'>Tajwid List</h1>
