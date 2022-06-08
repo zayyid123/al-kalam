6ea5d1373dda49546773f640b007324f163d0956
zayyid123 <mochamadzayyid@gmail.com>
2022-06-08 08:55:26 +0700

    add gh-pages

--- src/pages/bookmark/mobile/index.js
+++ src/pages/bookmark/mobile/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Mobile = () => {
     return (
-        <div className='lt:hidden w-full h-[100vh] bg-gradient-to-t from-[#a2ebfc] via-[#bfecf7] to-[#e0f6fa]'>
+        <div className='lt:hidden w-full h-[100vh] bg-gradient-to-t from-[#a2ebfc] via-[#bfecf7] to-[#DCE1C4]'>
             <h1>Mobile Bookmark</h1>
         </div>
     )
