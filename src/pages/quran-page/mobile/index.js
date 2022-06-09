0e2878c251d5a2bafcf880f25a337d4ea4084e08
zayyid123 <mochamadzayyid@gmail.com>
2022-06-09 18:52:36 +0700

    remove icon search

--- src/pages/quran-page/mobile/index.js
+++ src/pages/quran-page/mobile/index.js
@@ -6,10 +6,10 @@ import './style.css'
 // img
 import penandaHijau from '../../../assets/img/penanda-hijau.png'
 import penandaPutih from '../../../assets/img/penanda-putih.png'
-import iconSearch from '../../../assets/icons/img_icon_search.svg'
 
 const Mobile = () => {
     const [allData, setAllData] = useState([]);
+    const [valueSearch, setvalueSearch] = React.useState("");
 
     useEffect(() => {
         const getAllData = async () => {
@@ -20,47 +20,52 @@ const Mobile = () => {
         getAllData()
     }, [])
 
+    const handleChange = (event) => {
+        const value = event.target.value;
+        setvalueSearch(value);
+    };
+    React.useEffect(() => { }, [valueSearch]);
+
     return (
-        <div className='lt:hidden w-full bg-gradient-to-t from-[#a2ebfc] via-[#bfecf7] to-[#e8fbff] dark:from-[#211E2D] dark:via-[#211E2D] dark:to-[#211E2D] dark:text-white'>
+        <div className='lt:hidden min-h-full w-full bg-gradient-to-t from-[#a2ebfc] via-[#bfecf7] to-[#e8fbff] dark:from-[#211E2D] dark:via-[#211E2D] dark:to-[#211E2D] dark:text-white'>
             <NavbarMobile page={'Quran'} />
 
             <div className='flex justify-center items-center'>
-                <input className='dark:bg-[#2D2D3D] rounded-lg w-[70%] p-2 my-4' type='text' placeholder='Search' ></input>
-                <button className='rounded-lg bg-gray-900 ml-2 p-2'>
-                    <img src={iconSearch} alt='icon search' />
-                </button>
+                <input onChange={handleChange} className='input-search bg-[#cbeef7] dark:bg-[#2D2D3D] rounded-lg w-[70%] p-2 my-4' type='text' placeholder='Search' ></input>
             </div>
 
             {
-                allData.map((res, index) =>
-                    <div className='m-4' key={index + 'dataQuran'}>
-                        <div className='flex justify-between items-center'>
-                            <div className='flex justify-center items-center'>
-                                <div className='p-3 relative'>
-                                    <img className='dark:hidden w-[30px] h-[30px]' src={penandaHijau} alt={res.name + 'icon'} />
-                                    <img className='hidden dark:block w-[30px] h-[30px]' src={penandaPutih} alt={res.name + 'icon'} />
-                                    {
-                                        res.nomor < 10 ?
-                                            <p className='absolute z-10 bottom-[21px] left-[24px] text-[0.5rem]'>{res.nomor}</p>
-                                            :
-                                            res.nomor < 100 ?
-                                                <p className='absolute z-10 bottom-[21px] left-[22px] text-[0.5rem]'>{res.nomor}</p>
+                allData
+                    .filter((data) => data.nama_latin.toLowerCase().includes(valueSearch.toLowerCase()))
+                    .map((res, index) =>
+                        <div className='m-4' key={index + 'dataQuran'}>
+                            <div className='flex justify-between items-center'>
+                                <div className='flex justify-center items-center'>
+                                    <div className='p-3 relative'>
+                                        <img className='dark:hidden w-[30px] h-[30px]' src={penandaHijau} alt={res.name + 'icon'} />
+                                        <img className='hidden dark:block w-[30px] h-[30px]' src={penandaPutih} alt={res.name + 'icon'} />
+                                        {
+                                            res.nomor < 10 ?
+                                                <p className='absolute z-10 bottom-[21px] left-[24px] text-[0.5rem]'>{res.nomor}</p>
                                                 :
-                                                <p className='absolute z-10 bottom-[21px] left-[20px] text-[0.5rem]'>{res.nomor}</p>
-                                    }
+                                                res.nomor < 100 ?
+                                                    <p className='absolute z-10 bottom-[21px] left-[22px] text-[0.5rem]'>{res.nomor}</p>
+                                                    :
+                                                    <p className='absolute z-10 bottom-[21px] left-[20px] text-[0.5rem]'>{res.nomor}</p>
+                                        }
+                                    </div>
+                                    <div>
+                                        <h1 className='font-semibold text-lg'>{res.nama_latin}</h1>
+                                        <p className='font-thin text-gray-600 text-xs'>{res.tempat_turun === 'mekah' ? 'Makiyah' : 'Madaniyah'} - {res.jumlah_ayat} ayat</p>
+                                    </div>
                                 </div>
-                                <div>
-                                    <h1 className='font-semibold text-lg'>{res.nama_latin}</h1>
-                                    <p className='font-thin text-gray-500 text-xs'>{res.tempat_turun === 'mekah' ? 'Makiyah' : 'Madaniyah'} - {res.jumlah_ayat} ayat</p>
-                                </div>
+
+                                <h1 className='font-quran text-lg text-green-900 dark:text-white font-semibold' >{res.nama}</h1>
                             </div>
 
-                            <h1 className='font-quran text-lg text-green-900 dark:text-white font-semibold' >{res.nama}</h1>
+                            <hr />
                         </div>
-
-                        <hr />
-                    </div>
-                )
+                    )
             }
             <br /> <br /><br /><br />
         </div>
